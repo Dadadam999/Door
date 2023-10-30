1065b61593b3d3d7921a9b9c0b0e8c81cbe1d544
Dadadam <bogdanov.andrey.code@gmail.com>
2023-10-30 22:27:52 +0200

    Добавьте файлы проекта.

---
+++ Door.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Door
+{
+    public class Door
+    {
+        public delegate void ChangeStateHandler( Door sender );
+
+        public event ChangeStateHandler ChangeStateEvent;
+
+        private bool _open = false;
+        private bool _forbidden = false;
+
+        public (bool IsOpen, bool IsForbidden) States
+        {
+            get
+            {
+                return (_open, _forbidden);
+            }
+        }
+
+        public string Component { get; }
+
+        public Door( string component ) => Component = component;
+
+        public void Open()
+        {
+            if( _forbidden )
+            {
+                ChangeStateEvent( this );
+                Console.WriteLine( $"{Component} не открывается!" );
+                return;
+            }
+
+            _open = true;
+            ChangeStateEvent( this );
+            Console.WriteLine( $"{Component} открыта!" );
+        }
+
+        public void Close()
+        {
+            _open = false;
+            ChangeStateEvent( this );
+            Console.WriteLine( $"{Component} закрыта!" );
+        }
+
+        public void Lockup()
+        {
+            _forbidden = true;
+            ChangeStateEvent( this );
+            Console.WriteLine( $"{Component} заперта!" );
+        }
+
+        public void Unlock()
+        {
+            _forbidden = false;
+            ChangeStateEvent( this );
+            Console.WriteLine( $"{Component} отперта!" );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Door
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Door door = new Door("Деревянная дверь");
+            door.ChangeStateEvent += DoorChangeStateEvent;
+
+            door.Open();
+            door.Close();
+            door.Lockup();
+            door.Open();
+            door.Close();
+            door.Unlock();
+            door.Open();
+
+            Console.ReadKey();
+        }
+
+        private static void DoorChangeStateEvent( Door sender)
+        {
+            Console.WriteLine( $"{sender.Component} открыта: {sender.States.IsOpen} заперта {sender.States.IsForbidden}" );
+        }
+    }
+}
